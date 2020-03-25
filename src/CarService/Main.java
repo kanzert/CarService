6804c0345eb692f86ee2e25b9c045ea70affe161
Ziong <lezinx@corp.net>
2020-03-25 23:21:33 +0200

    Added BaseException class

--- src/CarService/Main.java
+++ src/CarService/Main.java
@@ -3,6 +3,10 @@ package CarService;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Log.setup();
+    }
+
+    public static void func() throws BaseAppException {
+
     }
 }
